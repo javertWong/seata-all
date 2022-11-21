bd4b63e1079820ec1ebb3b93d8574a64ab209d56
junkai.wang <junkai.wang@xinjifamily.com>
2022-11-21 14:58:28 +0800

    first commit

---
+++ src/test/java/com/jw/seataall/SeataAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.seataall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeataAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
